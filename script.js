fb97d4d8d15c63f545a661c54b74592d5281287e
saharjamali76 <56353997+saharjamali76@users.noreply.github.com>
2022-06-04 20:45:29 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Logical Operators
+// && and
+// || or
+// ! not
+
+// const age = 20;
+// const haslicense = false;
+// if (age >= 18 || haslicense) {
+//   console.log("she can drive");
+// } else {
+//   console.log("she can't drive yet");
+// }
+
+// console.log(true && true && false && true)
+// console.log(true && false)
+// console.log(false && false)
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || false)
+
+// const hour = 13;
+// if( hour < 10 || hour > 18 ){
+//     console.log("the company is closed")
+// }
+// else{
+//     console.log("the company is opened")
+// }
+
+// console.log(!false)
+
+// const isWeekend = false;
+// if( hour > 10 && hour < 18 && !isWeekend){
+//     console.log('the company is opened')
+// }else{
+//     console.log('the company is closed')
+
+// }
+
+// exercise
+
+const isLocation = true;
+const charge = 60;
+const storge = 10;
+if (isLocation && charge > 50 && storge >= 10) {
+  console.log("you can receive the update");
+} else {
+  console.log("you can't receive the update");
+}
